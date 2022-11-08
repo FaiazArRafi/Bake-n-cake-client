ae114bbb6c77741f5fdcad7d2db616782505c330
Faiaz <faiazarrafi@gmail.com>
2022-11-09 01:02:53 +0600

    favicon replaced

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,9 +1,50 @@
+import { Button, Navbar } from 'flowbite-react';
 import React from 'react';
 
 const Header = () => {
     return (
         <div>
-
+            <Navbar
+                fluid={true}
+                rounded={true}
+            >
+                <Navbar.Brand href="https://flowbite.com/">
+                    <img
+                        src="https://icon-library.com/images/cupcake-icon/cupcake-icon-3.jpg"
+                        className="mr-3 h-6 sm:h-9"
+                        alt="Flowbite Logo"
+                    />
+                    <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+                        Bake n Cake
+                    </span>
+                </Navbar.Brand>
+                <div className="flex md:order-2">
+                    <Button>
+                        Get started
+                    </Button>
+                    <Navbar.Toggle />
+                </div>
+                <Navbar.Collapse>
+                    <Navbar.Link
+                        href="/navbars"
+                        active={true}
+                    >
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        About
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Services
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Pricing
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Contact
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     );
 };
