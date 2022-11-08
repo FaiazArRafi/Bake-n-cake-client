b6c525193f1a535fb30bcf60e3dc5d23097e77ad
Faiaz <faiazarrafi@gmail.com>
2022-11-09 01:57:45 +0600

    authprovider completed

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -8,40 +8,37 @@ const Header = () => {
                 fluid={true}
                 rounded={true}
             >
-                <Navbar.Brand href="https://flowbite.com/">
+                <Navbar.Brand href="/">
                     <img
                         src="https://icon-library.com/images/cupcake-icon/cupcake-icon-3.jpg"
                         className="mr-3 h-6 sm:h-9"
-                        alt="Flowbite Logo"
+                        alt="Cake n Cake logo"
                     />
-                    <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+                    <span className="self-center whitespace-nowrap text-3xl font-semibold dark:text-white">
                         Bake n Cake
                     </span>
                 </Navbar.Brand>
                 <div className="flex md:order-2">
+                    <Button href='/login'>
+                        Login
+                    </Button>
                     <Button>
-                        Get started
+                        Sign Up
                     </Button>
                     <Navbar.Toggle />
                 </div>
                 <Navbar.Collapse>
                     <Navbar.Link
-                        href="/navbars"
+                        href="/"
                         active={true}
                     >
                         Home
                     </Navbar.Link>
-                    <Navbar.Link href="/navbars">
-                        About
+                    <Navbar.Link href="/services">
+                        Our Services
                     </Navbar.Link>
-                    <Navbar.Link href="/navbars">
-                        Services
-                    </Navbar.Link>
-                    <Navbar.Link href="/navbars">
-                        Pricing
-                    </Navbar.Link>
-                    <Navbar.Link href="/navbars">
-                        Contact
+                    <Navbar.Link href="/blog">
+                        Blog
                     </Navbar.Link>
                 </Navbar.Collapse>
             </Navbar>
